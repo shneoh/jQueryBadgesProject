89dee3c1e463bb03321f37a91177646060c48b83
shneoh <nelson@duaspace.com>
2017-11-18 13:00:07 +0800

    Add files via upload
    Code School Project #3

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2550161.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      addBadges(response.courses.completed);
+    }
+  });
 
+  function addBadges(courses){
+  	var $badges = $('#badges');
+
+  	courses.forEach(function(course){
+  		var $course = $('<div />',{
+  			'class' : 'course'
+  		}).appendTo($badges);
+
+  		$('<h3 />', {
+  			text: course.title
+  		}).appendTo($course);
+
+  		$('<img />', {
+  			src: course.badge
+  		}).appendTo($course);
+
+  		$('<a />', {
+  			href: course.url,
+  			target: '_blank',
+  			class: 'btn btn-primary',
+  			text: 'See Course'
+  		}).appendTo($course);
+  	})
+  }
 });
